cee866f2c2cb8b5fbf732481bd46ef562663984e
tapader13 <minhajtapader0@gmail.com>
2024-08-15 08:49:22 -0400

    fix search params 6

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -2,45 +2,44 @@
 import Card from '@/components/Card';
 import { Context } from '@/context/AppContext';
 import { fetchSearchData } from '@/lib/api';
-import React, { useContext, useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import React, { Suspense, useContext, useEffect, useState } from 'react';
 
 const SearchVIdeo = () => {
   const [data, setData] = useState([]);
   const context = useContext(Context);
+  const searchParams = useSearchParams();
+
+  const search = searchParams.get('query');
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const params = new URLSearchParams(window.location.search);
-      const query = params.get('query');
-
-      if (query) {
-        const fetchData = async () => {
-          try {
-            context.setProgress(30);
-            const fetchedData = await fetchSearchData(query);
-            setData(fetchedData.data);
-            context.setProgress(100);
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
-
-        fetchData();
+    const fetchData = async () => {
+      try {
+        context.setProgress(30);
+        const fetchedData = await fetchSearchData(search);
+        setData(fetchedData.data);
+        context.setProgress(100);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-    }
-  }, []);
+    };
+
+    fetchData();
+  }, [search]);
   console.log(data, 'srda');
   return (
-    <div
-      className={`${
-        context.toggles === true ? 'w-[90%]' : 'w-[85%]'
-      } text-white pt-[75px] max-h-screen  overflow-y-auto py-5`}
-      suppressHydrationWarning
-    >
-      {data.length > 0 &&
-        data?.map((data, i) => (
-          <Card key={i} identity='related' identity2='search' data={data} />
-        ))}
-    </div>
+    <Suspense fallback={<div className='text-white pt-[75px]'>Loading...</div>}>
+      <div
+        className={`${
+          context.toggles === true ? 'w-[90%]' : 'w-[85%]'
+        } text-white pt-[75px] max-h-screen  overflow-y-auto py-5`}
+        suppressHydrationWarning
+      >
+        {data.length > 0 &&
+          data?.map((data, i) => (
+            <Card key={i} identity='related' identity2='search' data={data} />
+          ))}
+      </div>
+    </Suspense>
   );
 };
 
